99fd194322d9f49ca01d8cab3036ba03c4bb1c72
Marina Dubovik <dubovik.m14@gmail.com>
2024-07-19 09:19:49 +0300

    feat: add logger to users service

--- users-microservice/src/main.ts
+++ users-microservice/src/main.ts
@@ -1,10 +1,17 @@
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
+    const logger = new Logger();
+
     const app = await NestFactory.create(AppModule);
+    const config = app.get<ConfigService>(ConfigService);
     app.useGlobalPipes(new ValidationPipe());
-    await app.listen(3000);
+
+    const port = config.get('PORT');
+    await app.listen(port, () => logger.log(`Server is listening on port ${port}`));
 }
+
 bootstrap();
--- users-microservice/src/users/users.service.ts
+++ users-microservice/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import * as bcrypt from 'bcrypt';
 import { CreateUserDto } from 'src/data/postgres/dto/create-user.dto';
@@ -9,6 +9,7 @@ import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
+    private readonly logger = new Logger(UsersService.name);
     constructor(
         @InjectRepository(User)
         private usersRepository: Repository<User>
@@ -29,33 +30,45 @@ export class UsersService {
     }
 
     async create(createUserDto: CreateUserDto): Promise<User> {
-        const isExists = await this.usersRepository.existsBy({ email: createUserDto.email });
-        if (isExists) throw new BadRequestException('User with such email already exixts');
+        try {
+            const isExists = await this.usersRepository.existsBy({ email: createUserDto.email });
+            if (isExists) throw new BadRequestException('User with such email already exixts');
 
-        createUserDto.password = await bcrypt.hash(createUserDto.password, 10);
+            createUserDto.password = await bcrypt.hash(createUserDto.password, 10);
 
-        return this.usersRepository.save(createUserDto);
+            return this.usersRepository.save(createUserDto);
+        } catch (error) {
+            this.logger.error(`Error while user creating: ${error.message}`);
+        }
     }
 
     async update(id: number, updateUserDto: UpdateUserDto): Promise<User> {
-        const user = await this.usersRepository.findOne({ where: { id } });
-        if (!user) throw new NotFoundException('User does not exist');
+        try {
+            const user = await this.usersRepository.findOne({ where: { id } });
+            if (!user) throw new NotFoundException('User does not exist');
 
-        if (updateUserDto.password) {
-            updateUserDto.password = await bcrypt.hash(updateUserDto.password, 10);
+            if (updateUserDto.password) {
+                updateUserDto.password = await bcrypt.hash(updateUserDto.password, 10);
+            }
+
+            return this.usersRepository.save({ ...user, ...updateUserDto });
+        } catch (error) {
+            this.logger.error(`Error while user updating: ${error.message}`);
         }
-
-        return this.usersRepository.save({ ...user, ...updateUserDto });
     }
 
     async delete(id: number): Promise<User> {
-        const user = await this.usersRepository.findOne({ where: { id } });
-        if (!user) throw new NotFoundException('User does not exist');
+        try {
+            const user = await this.usersRepository.findOne({ where: { id } });
+            if (!user) throw new NotFoundException('User does not exist');
 
-        const result = await this.usersRepository.delete({ id });
+            const result = await this.usersRepository.delete({ id });
 
-        if (result.affected > 0) {
-            return user;
+            if (result.affected > 0) {
+                return user;
+            }
+        } catch (error) {
+            this.logger.error(`Error while user deleting: ${error.message}`);
         }
     }
 }
